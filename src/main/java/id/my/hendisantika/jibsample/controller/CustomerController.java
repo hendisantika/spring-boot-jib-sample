6eccf74dad33937e886eb83a92591cd4f1d73a27
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 08:11:17 +0700

    feat: Add CustomerController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jibsample/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.jibsample.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jib-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/03/25
+ * Time: 08.11
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping(path = "api/v1/customers")
+public class CustomerController {
+}
