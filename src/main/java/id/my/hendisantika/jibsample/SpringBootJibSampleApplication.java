15d027c59b28eb9d991ae7167f02c9be495b4752
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 08:06:24 +0700

    feat: Init project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jibsample/SpringBootJibSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jibsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJibSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJibSampleApplication.class, args);
+    }
+
+}
