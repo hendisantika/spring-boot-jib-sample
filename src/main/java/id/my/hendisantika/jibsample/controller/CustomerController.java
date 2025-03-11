7f6f4546a1877979c2f638edade9dc291e6fc7b3
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 08:12:52 +0700

    feat: Add getMaleCustomers in CustomerController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/jibsample/controller/CustomerController.java
+++ src/main/java/id/my/hendisantika/jibsample/controller/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +29,16 @@ public class CustomerController {
     public List<Customer> getAllCustomers() throws Exception {
         List<Customer> customerList = MockData.getCustomers();
         return customerList;
+    }
+
+    @GetMapping("maleCustomers")
+    public List<Customer> getMaleCustomers() throws Exception {
+        List<Customer> customerList = MockData.getCustomers();
+        Predicate<Customer> customerPredicate = customer -> customer.getGender().equals("Male");
+
+        List<Customer> maleCustomers = customerList.stream()
+                .filter(customerPredicate)
+                .collect(Collectors.toList());
+        return maleCustomers;
     }
 }
--- src/main/java/id/my/hendisantika/jibsample/model/Customer.java
+++ src/main/java/id/my/hendisantika/jibsample/model/Customer.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.jibsample.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,8 +19,11 @@ import lombok.Setter;
 @Setter
 public class Customer {
     private Long id;
-    private String first_name;
-    private String last_name;
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
     private String email;
     private String gender;
     private String phone;
